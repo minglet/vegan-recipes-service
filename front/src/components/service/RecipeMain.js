a7ed8cfb38a470682f88559d74f7dc75b7978692
[AI5/1반] 권휘동 <gnlehd94613@gmail.com>
2022-10-12 20:15:24 +0900

    레시피 삭제 후 다시 코딩

---
+++ front/src/components/service/RecipeMain.js
@@ -0,0 +1,15 @@
+import Recipe from "./components/service/Recipe";
+
+function RecipeMain() {
+  return (
+    <>
+      <Recipe
+        title="Recipe Name"
+        imageUrl="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        body="This recipe is...."
+      />
+    </>
+  );
+}
+
+export default RecipeMain;
