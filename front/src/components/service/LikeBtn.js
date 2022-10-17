d10052b4813941b6004ce39524a991af3f93d2e8
Lizzie <leejihye7117@gmail.com>
2022-10-18 00:24:54 +0900

    recipeDetail like UI

--- front/src/components/service/LikeBtn.js
+++ front/src/components/service/LikeBtn.js
@@ -1,332 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styled from "@emotion/styled";
+import { Button } from "@mui/material";
 
 const Wrapper = styled("div")`
-  body {
-    margin: 0;
-    padding: 0;
-    -webkit-font-smoothing: antialiased;
-    font-family: arial;
-  }
-
-  #main-content {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    background: white;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  }
-
-  h1 {
-    font-size: 14px;
-    font-weight: 400;
-    margin: 10px 0 0 0;
-    color: #888888;
-  }
-
-  a {
-    font-size: 12px;
-    font-weight: bold;
-    margin-top: 10px;
-    display: inline-block;
-    text-decoration: none;
-    color: #008a68;
-  }
-
-  svg {
+  .button {
+    background: none;
+    border: none;
     cursor: pointer;
-    overflow: visible;
-    width: 60px;
-    #heart {
-      transform-origin: center;
-      animation: animateHeartOut 0.3s linear forwards;
-    }
-    #main-circ {
-      transform-origin: 29.5px 29.5px;
-    }
-  }
-
-  #checkbox {
-    display: none;
-  }
-
-  #checkbox:checked + label svg {
-    #heart {
-      transform: scale(0.2);
-      fill: #e2264d;
-      animation: animateHeart 0.3s linear forwards 0.25s;
-    }
-    #main-circ {
-      transition: all 2s;
-      animation: animateCircle 0.3s linear forwards;
-      opacity: 1;
-    }
-    #grp1 {
-      opacity: 1;
-      transition: 0.1s all 0.3s;
-      #oval1 {
-        transform: scale(0) translate(0, -30px);
-        transform-origin: 0 0 0;
-        transition: 0.5s transform 0.3s;
-      }
-      #oval2 {
-        transform: scale(0) translate(10px, -50px);
-        transform-origin: 0 0 0;
-        transition: 1.5s transform 0.3s;
-      }
-    }
-    #grp2 {
-      opacity: 1;
-      transition: 0.1s all 0.3s;
-      #oval1 {
-        transform: scale(0) translate(30px, -15px);
-        transform-origin: 0 0 0;
-        transition: 0.5s transform 0.3s;
-      }
-      #oval2 {
-        transform: scale(0) translate(60px, -15px);
-        transform-origin: 0 0 0;
-        transition: 1.5s transform 0.3s;
-      }
-    }
-    #grp3 {
-      opacity: 1;
-      transition: 0.1s all 0.3s;
-      #oval1 {
-        transform: scale(0) translate(30px, 0px);
-        transform-origin: 0 0 0;
-        transition: 0.5s transform 0.3s;
-      }
-      #oval2 {
-        transform: scale(0) translate(60px, 10px);
-        transform-origin: 0 0 0;
-        transition: 1.5s transform 0.3s;
-      }
-    }
-    #grp4 {
-      opacity: 1;
-      transition: 0.1s all 0.3s;
-      #oval1 {
-        transform: scale(0) translate(30px, 15px);
-        transform-origin: 0 0 0;
-        transition: 0.5s transform 0.3s;
-      }
-      #oval2 {
-        transform: scale(0) translate(40px, 50px);
-        transform-origin: 0 0 0;
-        transition: 1.5s transform 0.3s;
-      }
-    }
-    #grp5 {
-      opacity: 1;
-      transition: 0.1s all 0.3s;
-      #oval1 {
-        transform: scale(0) translate(-10px, 20px);
-        transform-origin: 0 0 0;
-        transition: 0.5s transform 0.3s;
-      }
-      #oval2 {
-        transform: scale(0) translate(-60px, 30px);
-        transform-origin: 0 0 0;
-        transition: 1.5s transform 0.3s;
-      }
-    }
-    #grp6 {
-      opacity: 1;
-      transition: 0.1s all 0.3s;
-      #oval1 {
-        transform: scale(0) translate(-30px, 0px);
-        transform-origin: 0 0 0;
-        transition: 0.5s transform 0.3s;
-      }
-      #oval2 {
-        transform: scale(0) translate(-60px, -5px);
-        transform-origin: 0 0 0;
-        transition: 1.5s transform 0.3s;
-      }
-    }
-    #grp7 {
-      opacity: 1;
-      transition: 0.1s all 0.3s;
-      #oval1 {
-        transform: scale(0) translate(-30px, -15px);
-        transform-origin: 0 0 0;
-        transition: 0.5s transform 0.3s;
-      }
-      #oval2 {
-        transform: scale(0) translate(-55px, -30px);
-        transform-origin: 0 0 0;
-        transition: 1.5s transform 0.3s;
-      }
-    }
-    #grp2 {
-      opacity: 1;
-      transition: 0.1s opacity 0.3s;
-    }
-    #grp3 {
-      opacity: 1;
-      transition: 0.1s opacity 0.3s;
-    }
-    #grp4 {
-      opacity: 1;
-      transition: 0.1s opacity 0.3s;
-    }
-    #grp5 {
-      opacity: 1;
-      transition: 0.1s opacity 0.3s;
-    }
-    #grp6 {
-      opacity: 1;
-      transition: 0.1s opacity 0.3s;
-    }
-    #grp7 {
-      opacity: 1;
-      transition: 0.1s opacity 0.3s;
-    }
-  }
-
-  @keyframes animateCircle {
-    40% {
-      transform: scale(10);
-      opacity: 1;
-      fill: #dd4688;
-    }
-    55% {
-      transform: scale(11);
-      opacity: 1;
-      fill: #d46abf;
-    }
-    65% {
-      transform: scale(12);
-      opacity: 1;
-      fill: #cc8ef5;
-    }
-    75% {
-      transform: scale(13);
-      opacity: 1;
-      fill: transparent;
-      stroke: #cc8ef5;
-      stroke-width: 0.5;
-    }
-    85% {
-      transform: scale(17);
-      opacity: 1;
-      fill: transparent;
-      stroke: #cc8ef5;
-      stroke-width: 0.2;
-    }
-    95% {
-      transform: scale(18);
-      opacity: 1;
-      fill: transparent;
-      stroke: #cc8ef5;
-      stroke-width: 0.1;
-    }
-    100% {
-      transform: scale(19);
-      opacity: 1;
-      fill: transparent;
-      stroke: #cc8ef5;
-      stroke-width: 0;
-    }
-  }
-
-  @keyframes animateHeart {
-    0% {
-      transform: scale(0.2);
-    }
-    40% {
-      transform: scale(1.2);
-    }
-    100% {
-      transform: scale(1);
-    }
-  }
-
-  @keyframes animateHeartOut {
-    0% {
-      transform: scale(1.4);
-    }
-    100% {
-      transform: scale(1);
-    }
   }
 `;
 
 export default function LikeBtn() {
+  const [isLiked, setIsLiked] = useState(true);
+
+  const onClick = () => {
+    setIsLiked(!isLiked);
+    // true일 경우 레시피카드를 myPage에 넘겨주게 ?
+    // console.log(isLiked);
+  };
   return (
     <Wrapper>
-      <div id="main-content">
-        <div>
-          <input type="checkbox" id="checkbox" />
-          <label for="checkbox">
-            <svg
-              id="heart-svg"
-              viewBox="467 392 58 57"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g
-                id="Group"
-                fill="none"
-                fill-rule="evenodd"
-                transform="translate(467 392)"
-              >
-                <path
-                  d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z"
-                  id="heart"
-                  fill="#AAB8C2"
-                />
-                <circle
-                  id="main-circ"
-                  fill="#E2264D"
-                  opacity="0"
-                  cx="29.5"
-                  cy="29.5"
-                  r="1.5"
-                />
-
-                <g id="grp7" opacity="0" transform="translate(7 6)">
-                  <circle id="oval1" fill="#9CD8C3" cx="2" cy="6" r="2" />
-                  <circle id="oval2" fill="#8CE8C3" cx="5" cy="2" r="2" />
-                </g>
-
-                <g id="grp6" opacity="0" transform="translate(0 28)">
-                  <circle id="oval1" fill="#CC8EF5" cx="2" cy="7" r="2" />
-                  <circle id="oval2" fill="#91D2FA" cx="3" cy="2" r="2" />
-                </g>
-
-                <g id="grp3" opacity="0" transform="translate(52 28)">
-                  <circle id="oval2" fill="#9CD8C3" cx="2" cy="7" r="2" />
-                  <circle id="oval1" fill="#8CE8C3" cx="4" cy="2" r="2" />
-                </g>
-
-                <g id="grp2" opacity="0" transform="translate(44 6)">
-                  <circle id="oval2" fill="#CC8EF5" cx="5" cy="6" r="2" />
-                  <circle id="oval1" fill="#CC8EF5" cx="2" cy="2" r="2" />
-                </g>
-
-                <g id="grp5" opacity="0" transform="translate(14 50)">
-                  <circle id="oval1" fill="#91D2FA" cx="6" cy="5" r="2" />
-                  <circle id="oval2" fill="#91D2FA" cx="2" cy="2" r="2" />
-                </g>
-
-                <g id="grp4" opacity="0" transform="translate(35 50)">
-                  <circle id="oval1" fill="#F48EA7" cx="6" cy="5" r="2" />
-                  <circle id="oval2" fill="#F48EA7" cx="2" cy="2" r="2" />
-                </g>
-
-                <g id="grp1" opacity="0" transform="translate(24)">
-                  <circle id="oval1" fill="#9FC7FA" cx="2.5" cy="3" r="2" />
-                  <circle id="oval2" fill="#9FC7FA" cx="7.5" cy="2" r="2" />
-                </g>
-              </g>
-            </svg>
-          </label>
-        </div>
-      </div>
+      <Button className="button" onClick={onClick}>
+        {isLiked ? (
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 1792 1792"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M320 1344q0-26-19-45t-45-19q-27 0-45.5 19t-18.5 45q0 27 18.5 45.5t45.5 18.5q26 0 45-18.5t19-45.5zm160-512v640q0 26-19 45t-45 19h-288q-26 0-45-19t-19-45v-640q0-26 19-45t45-19h288q26 0 45 19t19 45zm1184 0q0 86-55 149 15 44 15 76 3 76-43 137 17 56 0 117-15 57-54 94 9 112-49 181-64 76-197 78h-129q-66 0-144-15.5t-121.5-29-120.5-39.5q-123-43-158-44-26-1-45-19.5t-19-44.5v-641q0-25 18-43.5t43-20.5q24-2 76-59t101-121q68-87 101-120 18-18 31-48t17.5-48.5 13.5-60.5q7-39 12.5-61t19.5-52 34-50q19-19 45-19 46 0 82.5 10.5t60 26 40 40.5 24 45 12 50 5 45 .5 39q0 38-9.5 76t-19 60-27.5 56q-3 6-10 18t-11 22-8 24h277q78 0 135 57t57 135z"
+              fill="#bdbdbd"
+            />
+          </svg>
+        ) : (
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 1792 1792"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M320 1344q0-26-19-45t-45-19q-27 0-45.5 19t-18.5 45q0 27 18.5 45.5t45.5 18.5q26 0 45-18.5t19-45.5zm160-512v640q0 26-19 45t-45 19h-288q-26 0-45-19t-19-45v-640q0-26 19-45t45-19h288q26 0 45 19t19 45zm1184 0q0 86-55 149 15 44 15 76 3 76-43 137 17 56 0 117-15 57-54 94 9 112-49 181-64 76-197 78h-129q-66 0-144-15.5t-121.5-29-120.5-39.5q-123-43-158-44-26-1-45-19.5t-19-44.5v-641q0-25 18-43.5t43-20.5q24-2 76-59t101-121q68-87 101-120 18-18 31-48t17.5-48.5 13.5-60.5q7-39 12.5-61t19.5-52 34-50q19-19 45-19 46 0 82.5 10.5t60 26 40 40.5 24 45 12 50 5 45 .5 39q0 38-9.5 76t-19 60-27.5 56q-3 6-10 18t-11 22-8 24h277q78 0 135 57t57 135z"
+              fill="#37474f"
+            />
+          </svg>
+        )}
+      </Button>
     </Wrapper>
   );
 }
