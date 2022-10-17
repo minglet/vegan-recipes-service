75f6d4c993018c196c48485be06f747e5bc04c6e
Lizzie <leejihye7117@gmail.com>
2022-10-17 21:39:16 +0900

    recipeDetail data 연동 & UI

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
-
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import Main from "./components/mainPage/Main";
 // import UserPage from "./components/myPage/userPage/UserPage";
 import Recipes from "./components/service/ServiceRecipes";
+import ServiceRecipeDetail from "./components/service/ServiceRecipeDetail";
 import LoginPage from "./components/user/LoginPage";
 import RegisterPage from "./components/user/RegisterPage";
 // import teamPage from "./components/user/TeamPage";
@@ -13,14 +13,18 @@ import RegisterPage from "./components/user/RegisterPage";
 function App() {
   return (
     <Router>
-      <NavBar/>
-        <Routes>
-      {/* <UserPage /> */}
+      <NavBar />
+      <Routes>
+        {/* <UserPage /> */}
         {/* <Main /> */}
-        <Route path="/" element={<Main/>} />
-        <Route path="/recipes" element={<Recipes/>} />
-        <Route path="/user/login" element={<LoginPage/>} />
-        <Route path="/user/register" element={<RegisterPage/>} />
+        <Route path="/" element={<Main />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/user/login" element={<LoginPage />} />
+        <Route path="/user/register" element={<RegisterPage />} />
+        <Route
+          path="/recipes/current/:recipeId"
+          element={<ServiceRecipeDetail />}
+        />
         {/* <Route path="/user/teamPage" element={<TeamPage/>} /> */}
       </Routes>
     </Router>
--- front/src/components/service/ServiceRecipeDetail.js
+++ front/src/components/service/ServiceRecipeDetail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Button, Grid, Stack, Typography, CardMedia } from "@mui/material";
 import { useParams } from "react-router-dom";
+import LikeBtn from "./LikeBtn";
 import styled from "@emotion/styled";
 
 import * as Api from "../../api";
@@ -11,13 +12,11 @@ const Wrapper = styled("div")`
   display: flex;
   justify-content: center;
   flex-direction: column;
+  padding: 100px;
 
   .image-container {
+    top: 100px;
     margin: 20px;
-  }
-
-  .name-container {
-    margin: 50px;
   }
 
   .second-line-container {
@@ -26,16 +25,13 @@ const Wrapper = styled("div")`
     text-align: center;
     align-items: center;
     justify-content: space-between;
-    width: 700px;
-  }
-
-  .scroll-btn {
-    margin: 10px;
+    width: 50%;
+    margin: 30px;
   }
 
   .ingredients-container,
   .preparation-container {
-    margin-bottom: 1000px;
+    margin-bottom: 100px;
   }
 `;
 
@@ -43,51 +39,35 @@ export default function ServiceRecipeDetail() {
   const { recipeId } = useParams();
   const [recipes, setrecipes] = useState([]);
 
-  console.log(recipeId.image_url);
+  // console.log(recipes.title);
 
   useEffect(() => {
     if (!recipeId) return;
     // get요청res를 setState로 !
-    Api.get("recipes").then((res) => setrecipes(recipes));
+    Api.get(`recipes/current/${recipeId}`).then((res) => setrecipes(res.data));
   }, [recipeId]);
 
   return (
     <Wrapper>
       <div className="image-container">
-        <CardMedia
-          component="img"
-          height="auto"
-          width="600px"
-          image={recipes.image_url}
-        />
+        <CardMedia component="img" image={recipes.img_url} />
       </div>
       <div className="second-line-container">
         <div className="name-container">
-          <Typography variant="h3">title</Typography>
+          <Typography variant="h3">{recipes.title}</Typography>
         </div>
-        <div className="btn-container">
-          <Button
-            className="ingredients-scroll-btn"
-            variant="outlined"
-            size="small"
-          >
-            Ingredients
-          </Button>
-          <Button
-            className="preparation-scroll-btn"
-            variant="outlined"
-            size="small"
-          >
-            Preparation
-          </Button>
+        <div className="like-btn-container">
+          <LikeBtn />
         </div>
       </div>
 
       <div className="ingredients-container">
         <Typography variant="h5">Ingredients</Typography>
+        <Typography>{recipes.ingredients}</Typography>
       </div>
       <div className="preparation-container">
         <Typography variant="h5">Preparation</Typography>
+        <Typography>{recipes.preparation}</Typography>
       </div>
     </Wrapper>
   );
