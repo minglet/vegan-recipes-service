300ac3b4da0fa94e9a350ad23aa0cb687751f547
Lizzie <leejihye7117@gmail.com>
2022-10-19 02:52:08 +0900

    LikeBtn animation

--- front/src/components/service/LikeBtn.js
+++ front/src/components/service/LikeBtn.js
@@ -1,9 +1,10 @@
 import React, { useState, useContext, useEffect } from "react";
 import { UserStateContext, DispatchContext } from "../../App";
 import styled from "@emotion/styled";
-import { Button } from "@mui/material";
+import { Button, Paper } from "@mui/material";
 import * as Api from "../../api";
 import { createRoutesFromChildren } from "react-router-dom";
+import Grow from "@mui/material/Grow";
 import Recommend from "./Recommend";
 
 const Wrapper = styled("div")`
@@ -18,6 +19,16 @@ const Wrapper = styled("div")`
     top: 10vh;
     right: 155px;
     display: block;
+  }
+
+  .likeBtnImage {
+    transition: all 300ms;
+    &:active {
+      transform: scale(1.2);
+    }
+    path {
+      transition: all 500ms;
+    }
   }
 `;
 
@@ -51,15 +62,22 @@ export default function LikeBtn() {
 
   return (
     <Wrapper>
-      <div className="recommend-container">{isLiked && <Recommend />}</div>
+      <div className="recommend-container">
+        <Grow in={isLiked}>
+          <Paper sx={{ border: "none" }} elevation={0} variant="outlined">
+            <Recommend />
+          </Paper>
+        </Grow>
+      </div>
       <Button
         className="button"
         onClick={() => {
           handleClick();
         }}
       >
-        {isLiked ? (
+        {
           <svg
+            className="likeBtnImage"
             width="50"
             height="50"
             xmlns="http://www.w3.org/2000/svg"
@@ -67,22 +85,10 @@ export default function LikeBtn() {
           >
             <path
               d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
-              fill="#d32f2f"
+              fill={isLiked ? "#d32f2f" : "#bdbdbd"}
             />
           </svg>
-        ) : (
-          <svg
-            width="50"
-            height="50"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path
-              d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
-              fill="#bdbdbd"
-            />
-          </svg>
-        )}
+        }
       </Button>
     </Wrapper>
   );
