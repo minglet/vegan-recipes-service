e9e298db41d55d58c27214edff575de0b3dd841c
Lizzie <leejihye7117@gmail.com>
2022-10-19 01:53:07 +0900

    myPage add favorit Blank

---
+++ front/src/components/myPage/userPage/likeRecipes/Blank.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled("div")`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  margin: 10px;
+  width: 900px;
+  height: 500px;
+
+  .like-recipe-text {
+    display: inline-block;
+    background-color: #ffffff;
+    transform: translate(-300px, -32px);
+    padding: 0 10px 0 10px;
+  }
+`;
+
+export default function Blank() {
+  return (
+    <Wrapper>
+      <Typography variant="h4">
+        You don't have a favorite recipe yet!
+      </Typography>
+    </Wrapper>
+  );
+}
--- front/src/components/myPage/userPage/likeRecipes/RecipeCard.js
+++ front/src/components/myPage/userPage/likeRecipes/RecipeCard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Blank from "./Blank";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -41,47 +42,54 @@ export default function RecipeCard() {
     Api.get("recipes").then((res) => setrecipes(res.data));
   }, []);
 
+  // 상태가 true이면 Blank페이지, false면 좋아요한 레시피가 뜸
+  const [favorite, setFavorite] = useState(true);
+
   return (
     <Wrapper>
       <Container className="like-recipe-container" sx={{ py: 8 }} maxWidth="md">
         <Typography className="like-recipe-text">My favorite recipe</Typography>
         {/* End hero unit */}
         <Grid container spacing={4}>
-          {recipes.map((item) => (
-            <Grid item key={item} xs={12} sm={6} md={4}>
-              <Card
-                sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  image={item.img_url}
-                  height="250px"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {/* 레시피 이름 */}
-                    {item.title}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Grid xs="6">
-                    <Button size="small" color="inherit">
-                      <Link to={`/recipes/current/${item._id}`}>VIEW</Link>
-                    </Button>
-                  </Grid>
-                  <Grid xs="6">
-                    <Button size="small" color="inherit">
-                      DELETE
-                    </Button>
-                  </Grid>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
+          {favorite ? (
+            <Blank />
+          ) : (
+            recipes.map((item) => (
+              <Grid item key={item} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image={item.img_url}
+                    height="250px"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {/* 레시피 이름 */}
+                      {item.title}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Grid xs="6">
+                      <Button size="small" color="inherit">
+                        <Link to={`/recipes/current/${item._id}`}>VIEW</Link>
+                      </Button>
+                    </Grid>
+                    <Grid xs="6">
+                      <Button size="small" color="inherit">
+                        DELETE
+                      </Button>
+                    </Grid>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))
+          )}
         </Grid>
       </Container>
     </Wrapper>
