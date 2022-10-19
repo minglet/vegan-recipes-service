a728bdf9a2af2abbf980fb011e3fd11fe0411c4c
JBK <git.joe627@gmail.com>
2022-10-19 20:37:02 +0900

    회원정보수정 주석제거

--- front/src/components/myPage/userEditPage/UserEditPage.js
+++ front/src/components/myPage/userEditPage/UserEditPage.js
@@ -2,14 +2,9 @@ import { Link, useParams } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-// import Form from "@mui/material/Form";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-// import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import { useForm, Controller } from "react-hook-form";
 import * as Api from "../../../api";
-import { UserStateContext, DispatchContext } from "../../../App";
 import { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
 
@@ -27,16 +22,8 @@ export default function UserEditPage() {
 
   }, []);
 
-  console.log(user);
-  console.log(user.name);
-
-  // const { control, getValues } = useForm({
-  //   defaultValues: {
-  //     name: user.name,
-  //     password: user.password,
-  //     password_confirm: user.password,
-  //   },
-  // });
+  // console.log(user);
+  // console.log(user.name);
 
   // 비밀번호가 4글자 이상인지 여부를 확인함.
   const isPasswordValid = password.length >= 4;
@@ -55,8 +42,6 @@ export default function UserEditPage() {
       email: email,
       password: password,
     });
-
-    // 성공했을 때, name이랑 password를 해당 TextField 안에 있는 값으로 변경?
   };
 
   return (
@@ -75,61 +60,22 @@ export default function UserEditPage() {
           </Typography>
 
           <Box sx={{ mt: 3 }}>
-            {/* <Controller
-              control={control}
-              name="name"
-              rules={{
-                required: {
-                  value: true,
-                  message: "Please put your name",
-                },
-                minLength: {
-                  value: 2,
-                  message: "Name should be same as or more than 2 letters.",
-                },
-                maxLength: {
-                  value: 12,
-                  message: "Name cannot be more than 12 characters long.",
-                },
-              }}
-              render={({ field, fieldState: { error } }) => {
-                 return ( */}
                   <TextField
                     label="Name"
                     variant="outlined"
                     margin="normal"
                     fullWidth
                     autoFocus
-                    // {...field}
-                    // error={!!error}
-                    // helperText={error?.message}
                     defaultValue= {user.name}
                     value = {name}
                     onChange={(e) => setName(e.target.value)}
                   />
                   {!isNameValid && (
-                  <Form.Text className="text-success">
-                    Name should be same as or more than 2 letters.
-                  </Form.Text>
+                    <Form.Text className="text-success">
+                      Name should be same as or more than 2 letters.
+                    </Form.Text>
                   )}    
-              {/* )}}
-            /> */}
-
-            {/* <Controller
-              control={control}
-              name="password"
-              rules={{
-                required: {
-                  value: true,
-                  message: "put your password",
-                },
-                minLength: {
-                  value: 4,
-                  message: "Same as or More than 4 Letters.",
-                },
-              }}
-              render={({ field, fieldState: { error } }) => { */}
-                {/* return ( */}
+            
                   <TextField
                     name="password"
                     label="Password"
@@ -138,47 +84,15 @@ export default function UserEditPage() {
                     autoComplete="current-password"
                     margin="normal"
                     fullWidth
-                    // {...field}
-                    // error={!!error}
-                    // helperText={error?.message}
                     value = {password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
                   {!isPasswordValid && (
-                <Form.Text className="text-success">
-                  Same as or More than 4 Letters.
-                </Form.Text>
-              )}
-                {/* ); */}
-              {/* }}
-            /> */}
-
-            {/* <Controller
-              control={control}
-              name="password_confirm"
-              rules={{
-                // required: {
-                //   value: true,
-                //   message: "필수메세지",
-                // },
-                validate: (value) => {
-                  const password = getValues("password");
-                  if (password !== value) {
-                    return "Please check the password one more.";
-                  }
-                  return true;
-                },
-                onChange: (target) => {
-                  const { value } = target;
-                  const password = getValues("password");
-                  if (password !== value) {
-                    return "Please check the password one more.";
-                  }
-                  return true;
-                },
-              }}
-              render={({ field, fieldState: { error } }) => { */}
-                {/* return ( */}
+                    <Form.Text className="text-success">
+                      Same as or More than 4 Letters.
+                    </Form.Text>
+                  )}
+    
                   <TextField
                     label="Confirm password"
                     variant="outlined"
@@ -186,9 +100,6 @@ export default function UserEditPage() {
                     autoComplete="current-password"
                     margin="normal"
                     fullWidth
-                    // {...field}
-                    // error={!!error}
-                    // helperText={error?.message}
                     value = {password_confirm}
                     onChange={(e) => setPasswordConfirm(e.target.value)}
                   />
@@ -197,9 +108,6 @@ export default function UserEditPage() {
                       Please check the password one more.
                     </Form.Text>
                   )}
-                {/* ); */}
-              {/* }}
-            /> */}
 
             <Button
               // onClick={handleSubmit}
