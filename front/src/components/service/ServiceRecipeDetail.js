71fdaeda98913ec5cd50e01770706b65207f1e78
Lizzie <leejihye7117@gmail.com>
2022-10-19 10:05:38 +0900

    recipeDetaion title width

--- front/src/components/service/ServiceRecipeDetail.js
+++ front/src/components/service/ServiceRecipeDetail.js
@@ -32,7 +32,7 @@ const Wrapper = styled(Container)`
     flex-direction: column;
     text-align: center;
     align-items: center;
-    width: 50%;
+    width: 100%;
     /* margin: 30px; */
   }
 
