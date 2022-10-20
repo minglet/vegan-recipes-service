286b04c52f968d3cd571cff1481124aed20e519d
Lizzie <leejihye7117@gmail.com>
2022-10-20 10:49:58 +0900

    servicePage 정리

--- front/src/components/service/ServiceRecipes.js
+++ front/src/components/service/ServiceRecipes.js
@@ -11,69 +11,57 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import Spinner from "./Spinner";
 
 function ServiceRecipes(recipes) {
-
   const selectedRecipes = recipes.recipes;
 
   const limit = 6;
   const [page, setPage] = useState(1);
   const offset = (page - 1) * limit;
   return (
-    <Container>
-      { recipes.loading ? (
-        <Spinner />
-      ) : (
-        <Container
-          className="like-recipe-container"
-          sx={{ py: 8 }}
-          maxWidth="md"
-        >
-          <Grid container spacing={4}>
-            {selectedRecipes.slice(offset, offset + limit).map((item) => (
-              <Grid item key={item} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    image={item.img_url}
-                    alt="random"
-                    height="250px"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {/* 레시피 이름 */}
-                      {item.title}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Grid xs="6">
-                      <Button size="small" color="inherit">
-                        <Link to={`/recipes/current/${item._id}`}>VIEW</Link>
-                      </Button>
-                    </Grid>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+    <Container className="like-recipe-container" sx={{ py: 8 }} maxWidth="md">
+      <Grid container spacing={4}>
+        {selectedRecipes.slice(offset, offset + limit).map((item) => (
+          <Grid item key={item} xs={12} sm={6} md={4}>
+            <Card
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={item.img_url}
+                alt="random"
+                height="250px"
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {/* 레시피 이름 */}
+                  {item.title}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Grid xs="6">
+                  <Button size="small" color="inherit">
+                    <Link to={`/recipes/current/${item._id}`}>VIEW</Link>
+                  </Button>
+                </Grid>
+              </CardActions>
+            </Card>
           </Grid>
+        ))}
+      </Grid>
 
-          <footer>
-            <Pagination
-              total={selectedRecipes.length}
-              limit={limit}
-              page={page}
-              setPage={setPage}
-            />
-          </footer>
-         </Container>
-      )}
+      <footer>
+        <Pagination
+          total={selectedRecipes.length}
+          limit={limit}
+          page={page}
+          setPage={setPage}
+        />
+      </footer>
     </Container>
   );
 }
